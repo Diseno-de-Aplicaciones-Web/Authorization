3e36c7ef10d832ca6849b6561b9cc2118fd31e6d
DanBaDo <byte4life@pm.me>
2022-05-30 10:49:03 +0200

    First version: basic cookies

---
+++ Cookies/index.mjs
@@ -0,0 +1,20 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.use(cookieParser())
+
+app.get('/updatecookies/', (req,res)=>{
+    res.cookie("sessionId", Date.now().toString()) // Establecemos una cookie
+    res.cookie("idioma", "castrapo") // y otra más.
+    res.sendStatus(200)
+})
+
+app.get('/readcookies/', (req, res)=>{
+    const { sessionId, idioma} = req.cookies // Recuperamos las cookies
+    console.log(sessionId, idioma)
+    res.send(`Valor de las cookies: ${sessionId}, ${idioma}`)
+})
+
+app.listen(3000)
